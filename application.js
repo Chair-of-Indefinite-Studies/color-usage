231086ff485b268535931536688caa559fca647b
dvberkel <daan.v.berkel.1980@gmail.com>
2015-01-10 08:16:06 +0100

    use usage view

--- application.js
+++ application.js
@@ -1,9 +1,15 @@
 (function(color){
     var usage = new color.Usage();
 
+    new color.UsageView(usage, document.getElementById('colors'), function(element){
+	var color = element.getAttribute('data-entry');
+	element.textContent = color;
+	element.style.background = color;
+	element.classList.add('used', 'color', color);
+    });
+
     var colorInput = document.getElementById('color');
     document.getElementById('add').addEventListener('click', function(event){
 	usage.register(colorInput.value);
-	console.log(colorInput.value);
     });
 })(color);
